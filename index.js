441f4529be0a35499820f48ccdf249716c9a261a
Tomer Amir <tamir@vonage.com>
2017-05-08 07:57:22 +0300

    First working draft

---
+++ index.js
@@ -0,0 +1,274 @@
+'use strict';
+
+let path    = require('path');
+let express = require('express');
+const websocket = require('ws');
+let app = express();
+
+
+const wss = new websocket.Server({
+    perMessageDeflate: false,
+    port: 3001
+});
+
+let ws1, ws2;
+
+wss.on('connection', (ws) => {
+
+    if (!ws1) {
+        ws1 = ws;
+
+        ws.send(JSON.stringify({"response": "welcome", "data": "player1"}))
+    } else if (!ws2) {
+        ws2 = ws;
+
+        ws.send(JSON.stringify({"response": "welcome", "data": "player2"}))
+    }
+
+    if (ws1 && ws2) {
+        startGame();
+    }
+
+    broadcastGame();
+
+    myGame.state = 'progress';
+
+    ws.on('message', (data, flags) => {
+        let json;
+
+        try {
+            json = JSON.parse(data);
+        } catch (err) {
+            console.error(err);
+        }
+
+        switch(json.request) {
+            case 'move':
+                if (ws === ws1) {
+                    player1.speedX = json.speedX;
+                    player1.speedY = json.speedY;
+                } else if (ws === ws2) {
+                    player2.speedX = json.speedX;
+                    player2.speedY = json.speedY;
+                }
+
+                // updateGame();
+                break;
+        }
+
+        updateGame();
+    });
+
+    ws.on('close', () => {
+        if (ws === ws1) {
+            ws1 = undefined;
+            myGame.stopGame();
+        }
+
+        if (ws === ws2) {
+            ws2 = undefined;
+            myGame.stopGame();
+        }
+    });
+});
+
+function broadcastGame() {
+    wss.clients.forEach((curr) => {
+        curr.send(JSON.stringify(myGame));
+    });
+}
+
+
+let player1;
+let player2;
+let ball;
+let interval;
+
+let myGame = {
+    response: 'state',
+    pieces: [],
+    width: 480,
+    height: 270,
+    state: 'stopped',
+
+    start : function() {
+        this.state = 'start';
+        this.pieces = [];
+        this.width = 480;
+        this.height = 270;
+
+        interval = setInterval(updateGame, 20);
+    },
+
+    stopGame: function() {
+        this.state = 'stopped';
+        clearInterval(interval);
+    }
+};
+
+function startGame() {
+    myGame.start();
+
+    player1 = new component(15, 85, "red", 10, 97, 'player1', (curr) => {
+        if (curr.y <= 0) {
+            curr.y = 0;
+        }
+
+        if (curr.x <= 0) {
+            curr.x = 0;
+        }
+
+        if (curr.y >= (myGame.height - curr.height)) {
+            curr.y = (myGame.height - curr.height);
+        }
+
+        if (curr.x >= (myGame.width - curr.width)) {
+            curr.x = (myGame.width - curr.width);
+        }
+    });
+
+    player2 = new component(15, 85, "Blue", myGame.width - 25,  97, 'player2', (curr) => {
+        if (curr.y <= 0) {
+            curr.y = 0;
+        }
+
+        if (curr.x <= 0) {
+            curr.x = 0;
+        }
+
+        if (curr.y >= (myGame.height - curr.height)) {
+            curr.y = (myGame.height - curr.height);
+        }
+
+        if (curr.x >= (myGame.width - curr.width)) {
+            curr.x = (myGame.width - curr.width);
+        }
+    });
+
+    player1.speedYSize = 1;
+    player2.speedYSize = 1;
+
+    myGame.pieces.push(player1);
+    myGame.pieces.push(player2);
+
+    function playerBallCollision(player, ball) {
+
+    }
+
+    ball = new component(10, 10, "black", 235, 130, 'ball', (curr) => {
+        if (curr.x <= 0) {
+            clearInterval(interval);
+            myGame.state = 'done 1';
+
+            return;
+        }
+
+        if (curr.x >= myGame.width) {
+            clearInterval(interval);
+            myGame.state = 'done 2';
+
+            return;
+        }
+
+        if (curr.y <= 0 || curr.y >= (myGame.height - curr.height)) {
+            curr.changeY();
+        }
+
+        if (curr.x <= player1.x + player1.width) {
+            // if ((curr.y <= player1.y + player1.height && curr.y + curr.height > player1.y)
+            //     || (curr.y + curr.height >= player1.y && curr.y + curr.height < player1.y + player1.height)) {
+            //     curr.changeY();
+            // }
+
+            if (curr.y < player1.y + player1.height && curr.height + curr.y > player1.y) {
+                curr.changeX();
+            }
+        }
+
+        if (curr.x + curr.width >= player2.x) {
+            // if ((curr.y <= player2.y + player2.height && curr.y + curr.height > player2.y)
+            //     || (curr.y + curr.height >= player2.y && curr.y + curr.height < player2.y + player2.height)) {
+            //     curr.changeY();
+            // }
+
+            if (curr.y < player2.y + player2.height && curr.height + curr.y > player2.y) {
+                curr.changeX();
+            }
+        }
+    });
+    ball.speedXSize = 1;
+    ball.speedX = -1;
+    ball.speedYSize = 1;
+    ball.speedY = 1;
+
+    myGame.pieces.push(ball);
+}
+
+function component(width, height, color, x, y, name, collision) {
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedXSize = 0;
+    this.speedY = 0;
+    this.speedYSize = 0;
+    this.collision = collision;
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.name = name;
+
+    this.newPos = function() {
+        this.moveX();
+        this.moveY();
+
+        this.collision(this);
+    };
+
+    this.moveX = function() {
+        this.x += this.speedX * this.speedXSize;
+    };
+
+    this.moveY = function() {
+        this.y += this.speedY * this.speedYSize;
+    };
+
+
+    this.change = 0;
+
+    this.changeX = function() {
+        this.speedX *= -1;
+        this.moveX();
+
+        if (++this.change % 5 == 0)
+            this.speedXSize++;
+    };
+
+    this.changeY = function() {
+        this.speedY *= -1;
+        this.moveY();
+
+        if (++this.change % 5 == 0)
+            this.speedYSize++;
+    };
+}
+
+function updateGame() {
+
+    if (myGame.pieces)
+        myGame.pieces.forEach(function(p) {
+            p.newPos();
+        });
+
+    broadcastGame();
+}
+
+
+app.use('/', express.static(path.join(__dirname, 'static')));
+
+app.set('port', 3000);
+
+// Listen for requests
+let server = app.listen(app.get('port'), function() {
+    let port = server.address().port;
+    console.log('Magic happens on port ' + port);
+});
---
+++ static/script.js
@@ -0,0 +1,126 @@
+var ws;
+
+function startGame() {
+    var loc = window.location, new_uri;
+
+    if (loc.protocol === "https:") {
+        new_uri = "wss:";
+    } else {
+        new_uri = "ws:";
+    }
+    new_uri += "//" + loc.host;
+    new_uri = new_uri.substring(0, new_uri.length - 1) + "1";
+
+    ws = new WebSocket(new_uri + '/ws');
+
+    ws.onopen = function() {
+        console.log('connected');
+        myGameArea.start();
+    };
+
+    ws.onmessage = function(data) {
+        var json;
+
+        try {
+            json = JSON.parse(data.data);
+        } catch (err) {
+            console.error("Could not parse JSON: " + data + "\n", err);
+        }
+
+        switch(json.response) {
+            case 'welcome':
+                myGameArea.player_name = json.data;
+                break;
+
+            case 'state':
+                myGameArea.height = json.height;
+                myGameArea.width = json.width;
+                myGameArea.pieces = json.pieces;
+
+                myGameArea.pieces.forEach((p) => {
+                    if (p.name === 'player1') {
+                        myGameArea.player1 = p;
+                    } else if (p.name === 'player2') {
+                        myGameArea.player2 = p;
+                    } else if (p.name === 'ball') {
+                        myGameArea.ball = p;
+                    }
+                });
+
+                if (myGameArea.player_name === 'player1')
+                    myGameArea.player = myGameArea.player1;
+                else
+                    myGameArea.player = myGameArea.player2;
+
+                switch (json.state) {
+                    case 'stopped':
+                        break;
+                    case 'start':
+                        break;
+                    case 'progress':
+                        break;
+                }
+                myGameArea.state = json.state;
+
+                break;
+        }
+
+        updateGameArea();
+    };
+}
+
+function updateComponent(comp) {
+    ctx = myGameArea.context;
+    ctx.fillStyle = comp.color;
+    ctx.fillRect(comp.x, comp.y, comp.width, comp.height);
+
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.pieces = [];
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+
+        this.canvas.style = 'border: 1px solid black';
+
+        document.body.appendChild(this.canvas);
+        // this.interval = setInterval(updateGameArea, 20);
+
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+
+            // if (myGameArea.key == 37) {myGameArea.player.speedX = -1; }
+            // if (myGameArea.key == 39) {myGameArea.player.speedX = 1; }
+            if (myGameArea.key == 38) {myGameArea.player.speedY = -1; }
+            if (myGameArea.key == 40) {myGameArea.player.speedY = 1; }
+
+            ws.send(JSON.stringify({ request: "move", speedX: myGameArea.player.speedX, speedY: myGameArea.player.speedY }));
+        });
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = e.keyCode;
+            // if (myGameArea.key == 37) {myGameArea.player.speedX += 1; }
+            // if (myGameArea.key == 39) {myGameArea.player.speedX -= 1; }
+            if (myGameArea.key == 38) {myGameArea.player.speedY += 1; }
+            if (myGameArea.key == 40) {myGameArea.player.speedY -= 1; }
+
+            ws.send(JSON.stringify({ request: "move", speedX: myGameArea.player.speedX, speedY: myGameArea.player.speedY }));
+        });
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+};
+
+function updateGameArea() {
+    myGameArea.clear();
+
+    if (myGameArea.pieces)
+        myGameArea.pieces.forEach(function(p) {
+            updateComponent(p);
+        });
+}
+
+startGame();
